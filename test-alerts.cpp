ac957401f5d9873f356716841dbb9c580acfbd32
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-04-05 12:22:21 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,13 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits:") {
+TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+}
+
+TEST_CASE("Passive Cooling system with Normal temp and O/P fed to controller") {
+	Alert_Print_Status = NO_PRINT_DONE;
+	BatteryCharacter batteryCharacter = {PASSIVE_COOLING,"Brand_TCQ"};
+	REQUIRE(checkAndAlert(TO_CONTROLLER, batteryCharacter, 20) == NORMAL);
+	REQUIRE(Alert_Print_Status == PRINTED_IN_CONTROLLER);
 }
