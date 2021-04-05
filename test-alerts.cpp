3926e7d09c25bd999ceb5751b4051365bfee96eb
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-04-05 12:50:13 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,10 +3,6 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
-}
-
 TEST_CASE("Passive Cooling system with Normal temp and O/P fed to controller") {
 	
 	Alert_Status_s Alert_Status_Output = {NO_PRINT_DONE,DEFAULT_BREACH};
@@ -15,4 +11,24 @@ TEST_CASE("Passive Cooling system with Normal temp and O/P fed to controller") {
 	Alert_Status_Output = checkAndAlert(TO_CONTROLLER, batteryCharacter, 20);
 	REQUIRE(Alert_Status_Output.Alert_Print_Status == PRINTED_IN_CONTROLLER);
 	REQUIRE(Alert_Status_Output.breachType == NORMAL);
+}
+
+TEST_CASE("Passive Cooling system with Low temp and O/P fed to controller") {
+	
+	Alert_Status_s Alert_Status_Output = {NO_PRINT_DONE,DEFAULT_BREACH};
+	BatteryCharacter batteryCharacter = {PASSIVE_COOLING,"Brand_TCQ"};
+	
+	Alert_Status_Output = checkAndAlert(TO_CONTROLLER, batteryCharacter, 0);
+	REQUIRE(Alert_Status_Output.Alert_Print_Status == PRINTED_IN_CONTROLLER);
+	REQUIRE(Alert_Status_Output.breachType == TOO_LOW);
+}
+
+TEST_CASE("Passive Cooling system with High temp and O/P fed to controller") {
+	
+	Alert_Status_s Alert_Status_Output = {NO_PRINT_DONE,DEFAULT_BREACH};
+	BatteryCharacter batteryCharacter = {PASSIVE_COOLING,"Brand_TCQ"};
+	
+	Alert_Status_Output = checkAndAlert(TO_CONTROLLER, batteryCharacter, 60);
+	REQUIRE(Alert_Status_Output.Alert_Print_Status == PRINTED_IN_CONTROLLER);
+	REQUIRE(Alert_Status_Output.breachType == TOO_HIGH);
 }
