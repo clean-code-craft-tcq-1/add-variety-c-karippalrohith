c611ec5d7aa257c788b5e06ba7bef6744a2f479a
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-04-05 12:21:59 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -5,6 +5,8 @@ BatteryLimit_s BatteryLimit[3] = {{0,35},{0,45},{0,40}};
 printChargeLevel_Mail_s printChargeLevel_Mail[3] = {charge_Normal,charge_TooLow,charge_TooHigh};
 alert_Target_s alert_Target[3] = {sendToController,sendToEmail,sendToConsole};
 
+BreachType breachType_Status = DEFAULT;
+
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   if(value < lowerLimit) {
     return TOO_LOW;
@@ -20,26 +22,35 @@ BreachType classifyTemperatureBreach(CoolingType coolingType, double temperature
   return inferBreach(temperatureInC, BatteryLimit[coolingType].lowerLimit, BatteryLimit[coolingType].upperLimit);
 }
 
-void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) 
+BreachType checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) 
 {
   BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
 
 	alert_Target[alertTarget].alertTargetType(breachType);
+	
+	return breachType;
 }
 
-void sendToController(BreachType breachType) {
+void sendToController(BreachType breachType) 
+{
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
+  Alert_Print_Status = PRINTED_IN_CONTROLLER;
+  breachType_Status = breachType;
 }
 
 void sendToEmail(BreachType breachType) 
 {  
+  breachType_Status = breachType;
   printChargeLevel_Mail[breachType].printChargeLevel();
+  Alert_Print_Status = PRINTED_IN_EMAIL;
 }
 
 void sendToConsole(BreachType breachType) 
 {
   printf("%x\n", breachType);
+  breachType_Status = breachType;
+  Alert_Print_Status = PRINTED_IN_CONSOLE;
 }
 
 void charge_TooLow(void)
