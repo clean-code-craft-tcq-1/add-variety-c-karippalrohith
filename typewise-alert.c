aa43364b456ca62f9cf47a62ab7726f9ba987c70
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-04-05 12:46:09 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -4,8 +4,7 @@
 BatteryLimit_s BatteryLimit[3] = {{0,35},{0,45},{0,40}};
 printChargeLevel_Mail_s printChargeLevel_Mail[3] = {charge_Normal,charge_TooLow,charge_TooHigh};
 alert_Target_s alert_Target[3] = {sendToController,sendToEmail,sendToConsole};
-
-BreachType breachType_Status = DEFAULT_BREACH;
+Alert_Status_s Alert_Status = {NO_PRINT_DONE,DEFAULT_BREACH};
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   if(value < lowerLimit) {
@@ -22,35 +21,33 @@ BreachType classifyTemperatureBreach(CoolingType coolingType, double temperature
   return inferBreach(temperatureInC, BatteryLimit[coolingType].lowerLimit, BatteryLimit[coolingType].upperLimit);
 }
 
-BreachType checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) 
+Alert_Status_s checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) 
 {
   BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
 
 	alert_Target[alertTarget].alertTargetType(breachType);
 	
-	return breachType;
+	Alert_Status.breachType = breachType;
+	return Alert_Status;
 }
 
 void sendToController(BreachType breachType) 
 {
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
-  Alert_Print_Status = PRINTED_IN_CONTROLLER;
-  breachType_Status = breachType;
+  Alert_Status.Alert_Print_Status = PRINTED_IN_CONTROLLER;
 }
 
 void sendToEmail(BreachType breachType) 
 {  
-  breachType_Status = breachType;
   printChargeLevel_Mail[breachType].printChargeLevel();
-  Alert_Print_Status = PRINTED_IN_EMAIL;
+  Alert_Status.Alert_Print_Status = PRINTED_IN_EMAIL;
 }
 
 void sendToConsole(BreachType breachType) 
 {
   printf("%x\n", breachType);
-  breachType_Status = breachType;
-  Alert_Print_Status = PRINTED_IN_CONSOLE;
+  Alert_Status.Alert_Print_Status = PRINTED_IN_CONSOLE;
 }
 
 void charge_TooLow(void)
